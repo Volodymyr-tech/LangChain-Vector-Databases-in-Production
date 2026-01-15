eb205ae2057cc2f70535af4ed8941db84e9f0e2a
Valdemar <vovateslenko.1996.28@gmail.com>
2026-01-15 17:17:45 +0100

    Role prompting example

---
+++ README.md
@@ -0,0 +1,11 @@
+# LangChain Vector Databases in Production
+
+A collection of simple examples for working with Vector Databases using LangChain. 
+
+## Overview
+
+This project demonstrates how to:
+- Load and process documents (PDF, etc.).
+- Split text into optimized chunks.
+- Generate embeddings and store them in Vector DBs.
+- Perform similarity search and build RAG chains.
---
+++ src/prompt_templates.py
@@ -0,0 +1,29 @@
+from langchain_core.prompts import PromptTemplate
+
+from config.base import llm
+
+
+class RolePrompting():
+    model = llm
+    template = """
+                As a futuristic robot band conductor, I need you to help me come up with a song title.
+                What's a cool song title for a song about {theme} in the year {year}?
+                """
+
+    prompt = PromptTemplate(
+        input_variables=["theme", "year"],
+        template=template,
+    )
+
+    chain = prompt | model.chat
+
+    @classmethod
+    def get_response(cls, input_data:dict):
+        response = cls.chain.invoke(input_data)
+        print("Theme: interstellar travel")
+        print("Year: 3030")
+        print("AI-generated song title:", response.content)
+
+
+if __name__ == "__main__":
+    start = RolePrompting.get_response(input_data={"theme": "interstellar travel", "year": "3030"})
